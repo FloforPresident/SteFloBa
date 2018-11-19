ad93d5a4cdc0aaeb90397c9882348950a1331e3d
brunschba64196 <44465707+brunschba64196@users.noreply.github.com>
2018-11-19 16:20:28 +0100

    schießen

--- main-project/Assets/Skripts/NewBehaviourScript.cs
+++ main-project/Assets/Skripts/NewBehaviourScript.cs
@@ -5,16 +5,38 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour {
 
     float movespeed = 15f;
+    private bool schiessen = false;
+
+    public GameObject raketenPrefab;
+    public Transform RaketeSpawnPoint;
+    public float raketenSpeed = 500;
 
     // Use this for initialization
-    void Start () {
+    void Start ()
+    {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update()
+    {
         transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * movespeed,
                             Input.GetAxis("Vertical") * Time.deltaTime * movespeed,
                             0f);
+        if (Input.GetButtonDown("Fire1") && !schiessen)
+        {
+            schiessen = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if(schiessen)
+        {
+            GameObject Rakete = (GameObject) Instantiate(raketenPrefab, RaketeSpawnPoint.position, Quaternion.identity);
+            Rakete.GetComponent<Rigidbody2D>().AddForce(Vector3.up * raketenSpeed);
+
+            schiessen = false;
+        }
     }
 }
