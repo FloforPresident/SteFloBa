fe5f50db711d6e1ea2e5ab348d9b6e308a50ac30
FloforPresident <44464605+FloforPresident@users.noreply.github.com>
2018-11-19 14:38:25 +0100

    Add Project

---
+++ main-project/Assets/Skripts/NewBehaviourScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    float movespeed = 15f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * movespeed,
+                            Input.GetAxis("Vertical") * Time.deltaTime * movespeed,
+                            0f);
+    }
+}
