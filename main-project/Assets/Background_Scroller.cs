37b939a7d1ea76b41af0501692644ce0c625d548
Stefan95k <45176002+Stefan95k@users.noreply.github.com>
2018-11-21 21:29:38 +0100

    Hintergrund Autoscroll

---
+++ main-project/Assets/Background_Scroller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background_Scroller : MonoBehaviour {
+
+    public float speed;
+    Vector3 startPos;
+
+	// Use this for initialization
+	void Start () {
+        startPos = transform.position;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate((new Vector3(0, -1, 0)) * speed * Time.deltaTime);
+
+        if (transform.position.y < -12.19842)
+        {
+            transform.position = startPos;
+        }
+	}
+}
