4c99eeb342a95de45864304be8ff9ec575b575ca
Stefan95k <45176002+Stefan95k@users.noreply.github.com>
2018-11-22 16:33:30 +0100

    Antrieb animiert

---
+++ main-project/Assets/Antrieb_Bewegung.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Antrieb_Bewegung : MonoBehaviour {
+
+    float movespeed = 15f;
+
+    // Use this for initialization
+    void Start () {
+    }
+
+    // Update is called once per frame
+    void Update () {
+        //transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * movespeed, Input.GetAxis("Vertical") * Time.deltaTime * movespeed, 0f);
+       
+    }
+    void FixedUpdate()
+    {
+        var rigidBody = gameObject.GetComponent<Rigidbody2D>();
+        rigidBody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * movespeed, Input.GetAxisRaw("Vertical") * movespeed);
+    }
+}
+
