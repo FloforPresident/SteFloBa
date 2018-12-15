fc640e22bf011fb803027d41f8d17fc6afa73347
Stefan95k <45176002+Stefan95k@users.noreply.github.com>
2018-12-15 13:04:14 +0100

    Schild funktioniert
    Wills noch ändern, dass die Gegner komplett ignoriert werden

--- main-project/Assets/Skripts/Shield_Active_Handler.cs
+++ main-project/Assets/Skripts/Shield_Active_Handler.cs
@@ -4,25 +4,33 @@ using UnityEngine;
 
 public class Shield_Active_Handler : MonoBehaviour {
 
-  public static bool active;
+    public static bool active;
+    public GameObject shield;
+    uint framecounter = 0;
 
     // Use this for initialization
     void Start()
     {
-
+        shield.SetActive(false);
     }
 	
 	// Update is called once per frame
 	void Update () {
-
-  //funktioniert noch nicht, keine Ahnung warum
+        
         if (active == true)
         {
-            this.gameObject.SetActive(true);
+            shield.SetActive(true);
+            framecounter++;
         }
         else if (active == false)
         {
-            this.gameObject.SetActive(false);
+            shield.SetActive(false);
+        }
+
+        if (framecounter == 300)
+        {
+            active = false;
+            framecounter = 0;
         }
     }
 }
