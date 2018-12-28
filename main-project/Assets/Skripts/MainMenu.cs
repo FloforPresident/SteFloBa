1fbb52d85a5dc30342872a0890c2251e0971a56e
brunschba64196 <44465707+brunschba64196@users.noreply.github.com>
2018-12-28 16:49:25 +0100

    Haupt- und Steuerungsmenü

--- main-project/Assets/Skripts/MainMenu.cs
+++ main-project/Assets/Skripts/MainMenu.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour {
+public class MainMenu : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public GameObject gameOver;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+    }
+
+    public void EndGame()
+    {
+        Application.Quit();
+    }
+
 }
