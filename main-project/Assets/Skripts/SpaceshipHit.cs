0e5d2869cd834ff010bf6724d56f887dfe12a2a1
FloforPresident <44464605+FloforPresident@users.noreply.github.com>
2019-01-08 22:33:48 +0100

    Schrei eingefügt

--- main-project/Assets/Skripts/SpaceshipHit.cs
+++ main-project/Assets/Skripts/SpaceshipHit.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class SpaceshipHit : MonoBehaviour {
 
     public GameObject FeedbackBeiGetroffen;
+    AudioSource Audio;
+
+    void Start()
+    {
+        Audio = GetComponent<AudioSource>();
+    }
     void OnCollisionEnter2D(Collision2D col)
     {
         if (col.gameObject.tag.Equals("Enemy"))
         {
             GameControlScript.health -= 1;
+            Audio.Play();
             FeedbackBeiGetroffen.gameObject.SetActive(true);
         }
     }
